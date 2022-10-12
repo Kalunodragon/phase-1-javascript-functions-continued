cacb39366d16d82904cd3d229ba5a38652eb0650
Andrew <andrewonulak@gmail.com>
2022-10-12 16:31:49 -0600

    commit tree fd7d8456defe73d019b83f245657def975f45c6d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+const mondayWork = function(activity = 'go to the office'){
+    return (`This Monday, I will ${activity}.`)
+}
+function wrapAdjective(parameter = "special"){
+    return function(parameter2){
+        return (`You are ${parameter}${parameter2}${parameter}!`)
+        }
+}
